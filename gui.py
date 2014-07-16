4d07be89a78bc308bd64d406b52f5203f8575ce5
Adam Schwartz <adam@adamschwartz.io>
2014-07-15 23:39:53 -0500

    update gui.py

--- gui.py
+++ gui.py
@@ -10,44 +10,103 @@ class Dupy(QtGui.QWidget):
 
     def initUI(self):
         self.resize(600, 400)
-        self.center()
-        
+        # self.center()
+
+        self.setGeometry(1400, 250, 500, 400)  # set for development
+
         self.setAcceptDrops(True)
 
-        okButton = QtGui.QPushButton("OK")
-        cancelButton = QtGui.QPushButton("Cancel")
+        # Active Directories
+        self.addButton = QtGui.QPushButton("Add a directory")
+        self.addButton.clicked.connect(self.dirDialog)
 
-        title = QtGui.QLabel('Title')
-        author = QtGui.QLabel('Author')
-        review = QtGui.QLabel('Review')
+        descLable = QtGui.QLabel("...or drag and drop one into this window.")
 
-        titleEdit = QtGui.QLineEdit()
-        authorEdit = QtGui.QLineEdit()
-        self.reviewEdit = QtGui.QTextEdit()
-        self.reviewEdit.setReadOnly(True)
+        self.clearButton = QtGui.QPushButton("Clear")
+        self.clearButton.clicked.connect(self.clearDirs)
+        self.clearButton.hide()
 
-        grid = QtGui.QGridLayout()
-        grid.setSpacing(10)
+        self.pathEdit = QtGui.QTextEdit()
+        self.pathEdit.setReadOnly(True)
+        self.pathEdit.setMaximumHeight(100)
+        self.pathEdit.hide()
 
-        grid.addWidget(title, 1, 0)
-        grid.addWidget(titleEdit, 1, 1)
+        # Find Duplicates
+        self.findButton = QtGui.QPushButton("Find Duplicates")
+        self.findButton.hide()
 
-        grid.addWidget(author, 2, 0)
-        grid.addWidget(authorEdit, 2, 1)
+        # Duplicate Results
+        self.allLable = QtGui.QLabel('All Results')
+        self.allLable.hide()
 
-        grid.addWidget(review, 3, 0)
-        grid.addWidget(self.reviewEdit, 3, 1, 5, 1)
+        self.oneLable = QtGui.QLabel('dir one Results')
+        self.oneLable.hide()
 
-        self.setLayout(grid)
+        self.resultsEdit = QtGui.QTextEdit()
+        self.resultsEdit.setReadOnly(True)
+        self.resultsEdit.hide()
+
+        # Initial Layout
+        hbox = QtGui.QHBoxLayout(self)
+
+        leftSide = QtGui.QFrame(self)
+        rightSide = QtGui.QFrame(self)
+        splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
+        splitter.addWidget(leftSide)
+        splitter.addWidget(rightSide)
+
+        # Left Side Layout
+        leftGrid = QtGui.QGridLayout()
+        leftGrid.setSpacing(10)
+
+        leftGrid.addWidget(self.addButton, 2, 0)
+        leftGrid.addWidget(descLable, 2, 1)
+
+        leftGrid.addWidget(self.pathEdit, 3, 0, 1, 2)
+        leftGrid.addWidget(self.clearButton, 4, 0)
+
+        leftSide.setLayout(leftGrid)
+
+        # Left Side Layout
+        rightGrid = QtGui.QGridLayout()
+        rightGrid.setSpacing(10)
+
+        rightGrid.addWidget(self.findButton, 1, 0)
+        rightGrid.addWidget(self.allLable, 2, 0)
+        rightGrid.addWidget(self.oneLable, 2, 1)
+        rightGrid.addWidget(self.resultsEdit, 3, 0)
+
+        rightSide.setLayout(rightGrid)
+
+        # Set Parent Layout
+        hbox.addWidget(splitter)
+        self.setLayout(hbox)
 
         self.setWindowTitle('Dupy')
         self.show()
+
+    def mainLayout(self):
+        self.pathEdit.show()
+        self.clearButton.show()
+        self.findButton.show()
+        self.allLable.show()
+        self.oneLable.show()
+        self.resultsEdit.show()
 
     def center(self):
         qr = self.frameGeometry()
         cp = QtGui.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def dirDialog(self):
+        title = self.addButton.text()
+        dialogueDir = QtGui.QFileDialog.getExistingDirectory(self, title)
+        self.pathEdit.append(dialogueDir)
+        self.mainLayout()
+
+    def clearDirs(self):
+        self.pathEdit.setText("")
 
     # drag-and-drop directories into dupy
     def dragEnterEvent(self, event):
@@ -70,8 +129,8 @@ class Dupy(QtGui.QWidget):
 
             for url in event.mimeData().urls():
                 drop_path = str(url.toLocalFile())
-                # append path to textbox
-                self.reviewEdit.append(drop_path)
+                self.pathEdit.append(drop_path)  # add path to textbox
+                self.mainLayout()  # show the main layout
             self.emit(QtCore.SIGNAL("dropped"))
         else:
             event.ignore()
